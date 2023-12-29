8afb4622c931137fb97b6d6551c7e9a28f034a52
MartinMikkelsen <201706771@post.au.dk>
2023-12-29 16:24:14 +0100

    Added random tensor initialization in gauge.jl for tensor network computations

--- tensor_networks/Problems3.jl
+++ tensor_networks/Problems3.jl
@@ -11,3 +11,15 @@ for ni = 1:d
 end
 B = A;
 C = A;
+H = ncon(Any[A,B,C],Any[[-1,-2,1],[1,-3,2],[2,-4,-5]]);
+nH = norm(H)
+
+um,sm,vm = svd(reshape(C,d,d^2));
+chi = 2;
+CL = um[:,1:chi]*diagm(0 => sqrt.(sm[1:chi]));
+CR = reshape(diagm(0 => sqrt.(sm[1:chi]))*(vm[:,1:chi]'),chi,d,d);
+H1 = ncon(Any[A,B,CL,CR],Any[[-1,-2,1],[1,-3,2],[2,3],[3,-4,-5]]);
+ϵ = norm(H1-H)/nH
+
+# iterate QR decomps
+QA, RA = qr(reshape(A,d^2,d)); Ap = reshape(Matrix(QA),d,d,d);
